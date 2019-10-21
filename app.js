cab778890653b6e1d48a142b137cf1c907dcf47f
Zhivko Kanev <zh.kanev@protonmail.com>
2019-10-21 10:35:33 +0300

    logged an array of the placed cards

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-
 export default function solitaireGameInit(options) {
 
     const blankGrid = document.querySelector(".blank-grid");
     const stockBtn = document.querySelector("#stockBtn");
     const pack = [];
     let placeholderNumber = 0;
-    
+
     if (!(Array.isArray(options.suits)) || (options.suits.length === 0)) {
         options.suits = ["spades", "clubs", "diamonds", "hearts"];
         alert("Please, check suits array in your options. Given a default card values");
@@ -31,18 +30,20 @@ export default function solitaireGameInit(options) {
         alert("Please, check stockCount options. Given a default value of 52 cards");
     }
 
+
     stockBtn.style.cssText = "width: 150px; height: 45px; background-color: #999;";
+
     // Generate empty blank-grid 
-    for (rows = 0; rows < options.rowsCount; rows++) {
-        let row = document.createElement("div");
-        row.id = "row-" + (rows + 1);
+    for (let rows = 0; rows < options.rowsCount; rows++) {
+        const row = document.createElement("div");
+        row.id = `row - ${rows + 1}`;
         row.className = "row";
         row.style.cssText = "display: flex; justify-content: center; text-align: center; align-items: center;";
         blankGrid.appendChild(row);
 
-        for (col = 0; col < options.colsCount; col++) {
-            let placeholder = document.createElement("div");
-            placeholder.id = "placeholder-" + placeholderNumber;
+        for (let col = 0; col < options.colsCount; col++) {
+            const placeholder = document.createElement("div");
+            placeholder.id = `placeholder - ${placeholderNumber}`;
             placeholder.className = "placeholder";
             placeholder.style.cssText = "text-align: center; width: 70px; height: 90px;  border: 1px solid #555;";
             row.appendChild(placeholder);
@@ -52,79 +53,83 @@ export default function solitaireGameInit(options) {
 
     // Generate and shuffle the pack
     for (let i = 0; i < options.suits.length; i++) {
-        for (x = 0; x < options.values.length; x++) {
-            let singleCard = {
+        for (let x = 0; x < options.values.length; x++) {
+            const singleCard = {
                 Suit: options.suits[i],
                 Value: options.values[x]
             };
             pack.push(singleCard);
         }
     }
-  
 
-    function shuffle(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-          let j = Math.floor(Math.random() * (i + 1));
-          [array[i], array[j]] = [array[j], array[i]];
+    function shuffle(pack) {
+        for (let i = pack.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [pack[i], pack[j]] = [pack[j], pack[i]];
         }
-        return array;
+        return pack;
     }
 
     // Making the stock
-    let packLength = pack.length;
-    let stockLength = packLength - options.stockCount;
-    let stock = shuffle(pack);
+    const packLength = pack.length;
+    const stockLength = packLength - options.stockCount;
+    const stock = shuffle(pack);
     stock.splice(0, stockLength);
 
-
     // Main logic
-    document.getElementById("stockBtn").addEventListener('click', () => {
-        stockBtn.style.pointerEvents = "none";
+    stockBtn.addEventListener('click', appendPresentedCard);
+
+    function appendPresentedCard() {
 
         // Get random card from Array
-        let randomCard = stock[Math.floor(Math.random() * stock.length)];
+        const randomCard = stock[Math.floor(Math.random() * stock.length)];
 
         // Get index of the random card 
-        let randomCardIndex = stock.indexOf(randomCard);
+        const randomCardIndex = stock.indexOf(randomCard);
 
         // Splice this card from Array
         stock.splice(randomCardIndex, 1);
 
         // Append presented card to the DOM
-        let randomCardSuit = randomCard["Suit"];
-        let randomCardValue = randomCard["Value"];
+        const randomCardSuit = randomCard.Suit;
+        const randomCardValue = randomCard.Value;
 
         const presentedCard = document.createElement("p");
-            presentedCard.className = "card new-card " + `${randomCardSuit}`;
-            presentedCard.id = `${randomCardSuit}-${randomCardValue}`;
-            presentedCard.innerHTML = `<p>${randomCardValue}</p>  <p>${randomCardSuit}</p>`;
-            presentedCard.style.cssText = "position: absolute; text-align: center; width: 70px; height: 90px;  border: 1px solid #555;"
-        let newCard = document.getElementById("presented-card").appendChild(presentedCard);
+        presentedCard.className = `new-card ${randomCardSuit}`;
+        presentedCard.id = `${randomCardSuit}-${randomCardValue}`;
+        presentedCard.innerHTML = `<p>${randomCardValue}</p>  <p>${randomCardSuit}</p>`;
+        presentedCard.style.cssText = "position: absolute; text-align: center; width: 70px; height: 90px;  border: 1px solid #555;"
+        const newpresentedCard = document.getElementById("presented-card").appendChild(presentedCard);
 
-        const placeholders = document.querySelectorAll(".placeholder");
+        stockBtn.style.pointerEvents = "none";
+        blankGrid.style.pointerEvents = "auto";
+    }
 
-        placeholders.forEach((placeholder) => {
-            placeholder.parentElement.parentElement.style.pointerEvents = "auto";
-            placeholder.addEventListener('click', function () {
-            // new cards is presented until the stock is finished
-                if(stock.length || stock.length === 0) {
-                    applyCard(this);
-                } 
-                if(!stock.length) {
-                    stockBtn.disabled = true;
-                }
-            })
-        });
+    blankGrid.addEventListener('click', play);
 
-        // Apply presented card onto clicked placeholder
-        function applyCard(placeholder) {
-            placeholder.style.pointerEvents = "none";
-            placeholder.classList.remove('placeholder');
-            placeholder.parentElement.parentElement.style.pointerEvents = "none";
-            placeholder.className = "card " + `${randomCardSuit}`;
-            placeholder.innerHTML = `<p>${randomCardValue}</p><p>${randomCardSuit}</p>`;
+    function play(e) {
+        const newCard = document.querySelector(".new-card");
+        const { target } = e;
+
+        if (target.classList.contains('placeholder')) {
+            target.innerHTML = newCard.innerHTML;
+            newCard.remove();
             stockBtn.style.pointerEvents = "auto";
-            presentedCard.remove();
-        }        
-    });
+            blankGrid.style.pointerEvents = "none";
+            target.style.pointerEvents = "none";
+
+            if (!stock.length) {
+                stockBtn.disabled = true;
+                blankGrid.style.pointerEvents = "none";
+                const placeholders = [].slice.call(document.querySelectorAll('.placeholder'));
+                const dealtCardsArray = placeholders.map((placeholders) => placeholders.firstChild.innerHTML);
+                dealtCards(dealtCardsArray);
+            }
+        }
+    }
+
+    // Logs an array of the placed cards values once the grid is filled in
+    function dealtCards(dealtCardsArray) {
+        console.log(dealtCardsArray);
+    }
 }
